7a629067af223fc1ad99056c15ffaaa547e68692
Jan Erik Breimo <jebreimo@gmail.com>
2025-02-16 21:14:25 +0100

    Add grid2json.

---
+++ extras/grid2json/grid2json.cpp
@@ -0,0 +1,30 @@
+//****************************************************************************
+// Copyright © 2025 Jan Erik Breimo. All rights reserved.
+// Created by Jan Erik Breimo on 2025-02-11.
+//
+// This file is distributed under the Zero-Clause BSD License.
+// License text is included with the source distribution.
+//****************************************************************************
+#include <iostream>
+#include "GridLib/ReadGrid.hpp"
+#include "GridLib/WriteGrid.hpp"
+
+int main(int argc, char* argv[])
+{
+    if (argc != 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " FILENAME" << std::endl;
+        return 1;
+    }
+
+    try
+    {
+        write_json_grid(std::cout, GridLib::read_grid(argv[1]).view());
+    }
+    catch (std::exception& ex)
+    {
+        std::cerr << "Error: " << ex.what() << std::endl;
+        return 1;
+    }
+    return 0;
+}
