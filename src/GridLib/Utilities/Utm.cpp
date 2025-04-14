0108304a1a78ab94e1803340764ae6e2ced5f406
Jan Erik Breimo <jebreimo@gmail.com>
2025-04-14 23:37:26 +0200

    Update license info.

--- src/GridLib/Utilities/Utm.cpp
+++ src/GridLib/Utilities/Utm.cpp
@@ -1,3 +1,10 @@
+//****************************************************************************
+// Copyright © 2025 Jan Erik Breimo. All rights reserved.
+// Created by Jan Erik Breimo on 2025-02-16.
+//
+// This file is distributed under the Zero-Clause BSD License.
+// License text is included with the source distribution.
+//****************************************************************************
 #include <cmath>
 #include "Utm.hpp"
 
